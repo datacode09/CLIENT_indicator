bcf7fb3a606490eb295374e133e62378239b7e3c
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-03-18 12:25:31 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,13 +1,33 @@
 SELECT
-  o.client_number,
+  o.client_number_1,
   CASE
-    WHEN s.client_number IS NOT NULL THEN 'R'
-    WHEN h.client_number IS NOT NULL THEN 'H'
+    WHEN s1.client_number IS NOT NULL THEN 'R'
+    WHEN h1.client_number IS NOT NULL THEN 'H'
     ELSE 'Unknown'
-  END AS client_indicator
+  END AS client_indicator_1,
+  o.client_number_2,
+  CASE
+    WHEN s2.client_number IS NOT NULL THEN 'R'
+    WHEN h2.client_number IS NOT NULL THEN 'H'
+    ELSE 'Unknown'
+  END AS client_indicator_2,
+  o.client_number_3,
+  CASE
+    WHEN s3.client_number IS NOT NULL THEN 'R'
+    WHEN h3.client_number IS NOT NULL THEN 'H'
+    ELSE 'Unknown'
+  END AS client_indicator_3
 FROM
   Output_4b o
 LEFT JOIN
-  client_list s ON o.client_number = s.shared_client_numbers
+  client_list s1 ON o.client_number_1 = s1.shared_client_numbers
 LEFT JOIN
-  client_list h ON o.client_number = h.HSBC_client_numbers
+  client_list h1 ON o.client_number_1 = h1.HSBC_client_numbers
+LEFT JOIN
+  client_list s2 ON o.client_number_2 = s2.shared_client_numbers
+LEFT JOIN
+  client_list h2 ON o.client_number_2 = h2.HSBC_client_numbers
+LEFT JOIN
+  client_list s3 ON o.client_number_3 = s3.shared_client_numbers
+LEFT JOIN
+  client_list h3 ON o.client_number_3 = h3.HSBC_client_numbers
